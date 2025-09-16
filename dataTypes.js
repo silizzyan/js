692eca1df83876a0dd48b98400a4337464a387f2
silizzyan <edsiliizyaned@gmail.com>
2025-09-16 21:03:14 +0400

    Update dataTypes.js

--- dataTypes.js
+++ dataTypes.js
@@ -1,6 +1,58 @@
-// learn var / let / const
+// var / let / const
 
-// learn data types
+//var-y uni blokayin tesaneliutyun ev blokayinic durs tesaneliutyun orinak
+if(true){
+    var a = 2
+}
+console.log(a);
+2
 
-// Էդ ջան էս իմ գրածը կջնջես ու ստեղ կգրես բոլորը տիպերը, կսովորես var-ի ու let-ի const-ի տարբերությունը
-// սաղ ստեղ կգրես ու հենց գրես ինձ կասես
+//ev mek orinak evs erb hostingy tanum e verev
+console.log(x);
+var x = 5
+//js-y ayspes e kardum
+var x;
+console.log(x)
+x = 5
+//ayspes undefiend e tpelu bayc i tarberutyun mnacac popoxaknaeri na tesnum e vor trvac e mer x-y bayc trvac che arjeqy
+
+//let-y durs e ekel es6-um ev nra tarberutyun e vor orinak
+//let-y uni miayn blokayin tesaneliutyun
+if(true){
+    let a = 2
+}
+console.log(a);//ReferenceError: a is not defined
+//isk hostingy chi bardzracnum verev
+console.log(x);
+let x = 5
+
+//const-y chi popxum ir arjeqy nuynpes uni miayn blokayin tesaneliutyun
+//bayc erb menq const-um haytararum enq object/array menq chenq karox poxel hxumy/hascen bayc nersi tvyalnery karox enq poxel orinak
+const y = {name: "Edo"}
+y.name = "Ed" 
+//ayspes kashxati vorovhetev es popoxum em tvyaly
+//isk ayspes voch
+name = {name: "ed777"}
+
+//data types
+//tipery linum en primitiv ev obyektayin
+//parz tiper
+
+string = "text"
+number = 2342345
+boolean = true/false
+null = null
+undefiend = undefiend
+
+//obyektayin
+obj = {name: "sdasdf"} //object
+array = [1,2]//object
+regexp = /[A-Z]/g // object
+function = function name(){}//object
+
+
+
+
+
+
+
